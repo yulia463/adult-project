e02e951f90c893571dd23d1dd4209eda90f2d5f0
yuliaslatvitskaya <yuli.slat@yandex.ru>
2025-12-12 15:46:43 +0100

    feat: add second screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import MainScreen from "@/components/MainScreen/MainScreen";
+import TableOfContents from "@/components/TableOfContents/TableOfContents";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-      <div>
+      <div className={styles.mainColor}>
         <MainScreen/>
+          <TableOfContents/>
       </div>
   );
 }
--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -1,8 +1,39 @@
-import styles from "@/components/MainScreen/MainScreen.module.scss";
+import styles from "@/components/TableOfContents/TableOfContents.module.scss";
+import Button from "@/components/Button";
+import BigRocket from "@/app/svg/BigRocket";
 
 export default function TableOfContents() {
     return (
-        <div className={styles.container}>
+        <div className={styles.containerTableOfContents}>
+            <div>Table of Contents</div>
+            <div>Undress her free: How It Works?</div>
+            <div className={styles.whiteContainer}>
+                <div className={styles.descriptionOfContainer}>
+                    <div className={styles.textUndress}>Undress her free:</div>
+                    <div className={styles.textWorks}>How It Works?</div>
+                </div>
+
+                <div>
+                    Our service is a sophisticated AI-powered app that learns from large amounts of online data. Using
+                    complex algorithms, it can remove clothing in seconds thanks to its precise analysis of the human
+                    body.
+                    Our platform provides our users with a smooth undressing process, offering free undress photo.
+                </div>
+                <div>
+                    <div>icon</div>
+                    <div>A secure and innovative deepnude app</div>
+                    <div>icon</div>
+                    <div>Confidential registration with full data protection</div>
+                </div>
+                <div>
+                    <Button size="M" variant="pink" icon={BigRocket}>Undress a photo</Button>
+                </div>
+                <div>All you need is an internet connection and the photo you want to undress. It is very simple for
+                    you, without any unnecessary steps.
+                </div>
+                <div>foto</div>
+                <div>line</div>
+            </div>
         </div>
     );
 }
